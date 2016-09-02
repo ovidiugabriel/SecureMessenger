a14a3fcadf11195aa29942f8191678171c5af1fa
Ovidiu <ovidiugabriel@gmail.com>
2016-09-02 14:12:58 +0300

    Create safestr.c

---
+++ safe/safestr.c
@@ -0,0 +1,16 @@
+
+#include "safestr.h"
+
+safestr_t SAFESTR_Make(int size)
+{
+    safestr_t safeString;
+    safeString.mBuffer = (char*) calloc(size);
+    safeString.mSize = (safeString.mBuffer) ? size : 0;
+    safeString.mLength = 0;
+    return safeString;
+}
+
+int SAFESTR_Length(const safestr_t* pSafeString)
+{
+    return pSafeString->mLength;
+}
