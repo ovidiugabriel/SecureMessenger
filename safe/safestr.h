01282f448ae4866a5fac53b87337036ce6d043aa
Ovidiu <ovidiugabriel@gmail.com>
2016-09-02 14:06:16 +0300

    Update safestr.h

--- safe/safestr.h
+++ safe/safestr.h
@@ -6,6 +6,7 @@ struct SAFESTR {
     int mLength;
     char* buffer;
 };
+typedef struct SAFESTR safestr_t;
 
 //
 // Initial Source: https://zone.ni.com/reference/en-XX/help/370051V-01/cvi/libref/cviformatting_and_io_library_functi/
@@ -18,43 +19,20 @@ struct SAFESTR {
  * 
  * But a negative number will be returned instead.
  */
-int safe_StringLength(const char string[], int maxlen);
-int SAFESTR_Length(const struct SAFESTR* pStr);
+int SAFESTR_Length(const safestr_t* pStr);
 
-int safe_StringLowerCase(char* destString, int destStringSize,
-    const char* sourceString, int sourceStringSize );
-int SAFESTR_LowerCase(struct SAFESTR* dest, const struct SAFESTR* src);
-    
-int safe_StringUpperCase(char* destString, int destStringSize,
-    const char* sourceString, int sourceStringSize );
-int SAFESTR_UpperCase(struct SAFESTR* dest, const struct SAFESTR* src);
-
-int safe_FillBytes(char* buffer, int bufferSize, int startingIndex, int numberOfBytes, unsigned char value);
-int SAFESTR_FillBytes(struct SAFESTR* buffer, int startingIndex, int numberOfBytes, unsigned char value);
-
-int safe_CopyBytes(char* targetBuffer int targetBufferSize, int targetIndex, 
-    const char* sourceBuffer int sourceBufferSize, int sourceIndex, int numberOfBytes);
-int SAFESTR_CopyBytes(struct SAFESTR* target, int tagetIndex,
-    const struct SAFESTR* source, int sourceIndex, int numberOfBytes );
-
-int safe_CopyString(char* targetString int targetStringSize, int targetIndex, 
-    const char* sourceString, int sourceStringSize, int sourceIndex, int maximumBytes);
-int SAFESTR_CopyString(struct SAFESTR* target, int targetIndex,
-    const struct SAFESTR* source, int sourceIndex, int maximumBytes);
-
-int safe_CompareBytes(const char* buffer1 int buffer1Size, int buffer1Index, 
-    const char* buffer2, int buffer2Size, int buffer2Index, int numberOfBytes, int caseSensitive);
-int SAFESTR_CompareBytes(struct SAFESTR* buffer1, int buffer1Index,
-    const struct SAFESTR* buffer2, int buffer2Index, int numberOfBytes, int caseSensitive);
-
-int safe_CompareStrings(const char* string1, int string1Size, int string1Index, 
-    const char* string2, int string2Size, int string2Index, int caseSensitive);
-int SAFESTR_CompareStrings(const struct SAFESTR* string1, int string1Index,
-    const struct SAFESTR* string2, int string2Index, int caseSensitive);
-    
-int safe_FindPattern(const char* buffer, int bufferSize, int startingIndex, int numberOfBytes, 
-    const char* pattern, int patternSize, int caseSensitive, int startFromRight);
-int SAFESTR_FindPattern(const struct SAFESTR* buffer, int startingIndex, int numberOfBytes,
-    const struct SAFESTR* pattern, int caseSensitive, int startFromRight);
+int SAFESTR_LowerCase(safestr_t* dest, const safestr_t* src);
+int SAFESTR_UpperCase(safestr_t* dest, const safestr_t* src);
+int SAFESTR_FillBytes(safestr_t* buffer, int startingIndex, int numberOfBytes, unsigned char value);
+int SAFESTR_CopyBytes(safestr_t* target, int tagetIndex,
+    const safestr_t* source, int sourceIndex, int numberOfBytes );
+int SAFESTR_CopyString(safestr_t* target, int targetIndex,
+    const safestr_t* source, int sourceIndex, int maximumBytes);
+int SAFESTR_CompareBytes(safestr_t* buffer1, int buffer1Index,
+    const safestr_t* buffer2, int buffer2Index, int numberOfBytes, int caseSensitive);
+int SAFESTR_CompareStrings(const safestr_t* string1, int string1Index,
+    const safestr_t* string2, int string2Index, int caseSensitive);
+int SAFESTR_FindPattern(const safestr_t* buffer, int startingIndex, int numberOfBytes,
+    const safestr_t* pattern, int caseSensitive, int startFromRight);
 
 #endif
