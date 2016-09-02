a7815be0d5ff0e3b4dd8781941dbaf96363ef896
Ovidiu <ovidiugabriel@gmail.com>
2016-09-02 14:15:49 +0300

    Update safestr.c

--- safe/safestr.c
+++ safe/safestr.c
@@ -7,13 +7,15 @@ struct SAFESTR {
     char* mBuffer;
 };
 
-safestr_t SAFESTR_Make(int size)
+safestr_t* SAFESTR_Make(safestr_t* pSafeString int size)
 {
-    safestr_t safeString;
-    safeString.mBuffer = (char*) calloc(size);
-    safeString.mSize = (safeString.mBuffer) ? size : 0;
-    safeString.mLength = 0;
-    return safeString;
+    pSafeString->mBuffer = (char*) calloc(size);
+    pSafeString->mSize = (pSafeString->mBuffer) ? size : 0;
+    pSafeString->mLength = 0;
+    if (pSafeString->mBuffer) {
+        return pSafeString;
+    }
+    return NULL;
 }
 
 int SAFESTR_Length(const safestr_t* pSafeString)
