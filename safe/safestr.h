99e34fdb516c02a8c8a4000c75c3e6414df09c85
Ovidiu <ovidiugabriel@gmail.com>
2016-09-02 13:52:41 +0300

    Update and rename formatio.h to safestr.h

--- safe/safestr.h
+++ safe/safestr.h
@@ -1,7 +1,7 @@
-#ifndef SAFE_FORMATIO_H
-#define SAFE_FORMATIO_H
+#ifndef SAFESTR_H
+#define SAFESTR_H
 
-struct STR {
+struct SAFESTR {
     int mSize;
     int mLength;
     char* buffer;
@@ -19,23 +19,33 @@ struct STR {
  * But a negative number will be returned instead.
  */
 int safe_StringLength(const char string[], int maxlen);
+int SAFESTR_Length(const struct SAFESTR* pStr);
 
 int safe_StringLowerCase(char* destString, int destStringSize,
     const char* sourceString, int sourceStringSize );
+int SAFESTR_LowerCase(struct SAFESTR* dest, const struct SAFESTR* src);
     
 int safe_StringUpperCase(char* destString, int destStringSize,
     const char* sourceString, int sourceStringSize );
+int SAFESTR_UpperCase(struct SAFESTR* dest, const struct SAFESTR* src);
 
 int safe_FillBytes(char* buffer, int bufferSize, int startingIndex, int numberOfBytes, unsigned char value);
+int SAFESTR_FillBytes(struct SAFESTR* buffer, int startingIndex, int numberOfBytes, unsigned char value);
 
 int safe_CopyBytes(char* targetBuffer int targetBufferSize, int targetIndex, 
     const char* sourceBuffer int sourceBufferSize, int sourceIndex, int numberOfBytes);
+int SAFESTR_CopyBytes(struct SAFESTR* target, int tagetIndex,
+    const struct SAFESTR* source, int sourceIndex, int numberOfBytes );
 
 int safe_CopyString(char* targetString int targetStringSize, int targetIndex, 
     const char* sourceString, int sourceStringSize, int sourceIndex, int maximumBytes);
+int SAFESTR_CopyString(struct SAFESTR* target, int targetIndex,
+    const struct SAFESTR* source, int sourceIndex, int maximumBytes);
 
 int safe_CompareBytes(const char* buffer1 int buffer1Size, int buffer1Index, 
     const char* buffer2, int buffer2Size, int buffer2Index, int numberOfBytes, int caseSensitive);
+int SAFESTR_CompareBytes(struct SAFESTR* buffer1, int buffer1Index,
+    const struct SAFESTR* buffer2, int buffer2Index, int numberOfBytes, int caseSensitive);
 
 int safe_CompareStrings(const char* string1, int string1Size, int string1Index, 
     const char* string2, int string2Size, int string2Index, int caseSensitive);
